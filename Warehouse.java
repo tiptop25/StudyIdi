e4d0741ede3245ee1a8db3d0f79ce1ab471c8e6f
tiptop25 <melnik_af@mail.ru>
2015-06-26 15:43:14 +0300

    интернет магазин

---
+++ Warehouse.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by superova on 19.06.2015.
+ */
+public class Warehouse {
+ int namber;
+    int area;
+    String address;
+    String materiallyResponsible;
+
+
+    public Warehouse(int namber, int area, String address, String materiallyResponsible) {
+        this.namber = namber;
+        this.area = area;
+        this.address = address;
+        this.materiallyResponsible = materiallyResponsible;
+
+    }
+}
+
